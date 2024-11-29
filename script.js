67adf6f63f5cd193245d9cae04f251361d2851c9
Michael Aguas <sensei.ml.me@gmail.com>
2024-11-28 22:06:46 -0500

    Fix toggle function

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function toggleCollapse(contentId) {
     const content = document.getElementById(contentId);
     const isVisible = content.style.display === 'block';
@@ -15,21 +14,3 @@ function toggleCollapse(contentId) {
         content.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
 }
-=======
-function toggleCollapse(contentId) {
-    const content = document.getElementById(contentId);
-    const isVisible = content.style.display === 'block';
-
-    // Colapsar todos los contenidos
-    const allContents = document.querySelectorAll('.content');
-    allContents.forEach((item) => {
-        item.style.display = 'none';
-    });
-
-    // Si el contenido no era visible, mostrarlo y hacer scroll
-    if (!isVisible) {
-        content.style.display = 'block';
-        content.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-}
->>>>>>> 6d7455b1e2ff10c4895ec8986ad7dc87e33b743a
